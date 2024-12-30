08159e935600b7e7231beb75ee0e9644a10af1a7
Visha765 <lewis.ap75@gmail.com>
2024-12-30 14:03:29 +0900

    feat: .gitconfig

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,20 +1,23 @@
-DOT = $HOME/dotfiles
+#!/bin/bash
 
-# $HOME
-ln -fs $DOT/zsh/.zshrc $HOME/.zshrc
-ln -fs $DOT/tmux/.tmux $HOME/.tmux
-ln -fs $DOT/tmux/.tmux.conf $HOME/.tmux.conf
-ln -fs $DOT/fzf/.fzf.zsh $HOME/.fzf.zsh
+# DOTFILESのパスを設定
+DOT="$HOME/dotfiles"
 
-# .config
-CONF = $HOME/.config
+# ホームディレクトリの設定ファイル
+ln -fs "$DOT/zsh/.zshrc" "$HOME/.zshrc"
+ln -fs "$DOT/tmux/.tmux" "$HOME/.tmux"
+ln -fs "$DOT/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -fs "$DOT/fzf/.fzf.zsh" "$HOME/.fzf.zsh"
+ln -fs "$DOT/.gitconfig" "$HOME/.gitconfig"
 
-ln -fs $DOT/nvim $CONF/nvim
-ln -fs $DOT/sheldon $CONF/sheldon
-ln -fs $DOT/wezterm $CONF/wezterm
-ln -fs $DOT/starship.toml $CONF/starship.toml
+# .configディレクトリの設定ファイル
+CONF="$HOME/.config"
 
+# .configディレクトリが存在しない場合は作成
+mkdir -p "$CONF"
 
-
-
+ln -fs "$DOT/nvim" "$CONF/nvim"
+ln -fs "$DOT/sheldon" "$CONF/sheldon"
+ln -fs "$DOT/wezterm" "$CONF/wezterm"
+ln -fs "$DOT/starship.toml" "$CONF/starship.toml"
 
