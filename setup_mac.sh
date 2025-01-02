1a73815256bee9194e6bd48a7ff638e252514c59
Visha765 <lewis.ap75@gmail.com>
2025-01-02 14:44:28 +0900

    fix: setup command

--- setup_mac.sh
+++ setup_mac.sh
@@ -4,11 +4,25 @@
 DOT="$HOME/dotfiles"
 
 # ホームディレクトリの設定ファイル
-ln -fs "$DOT/zsh/.zshrc" "$HOME/.zshrc"
-ln -fs "$DOT/tmux/.tmux" "$HOME/.tmux"
-ln -fs "$DOT/tmux/.tmux.conf" "$HOME/.tmux.conf"
-ln -fs "$DOT/fzf/.fzf.zsh" "$HOME/.fzf.zsh"
-ln -fs "$DOT/.gitconfig" "$HOME/.gitconfig"
+create_symlink() {
+    src=$1
+    dest=$2
+
+    # 既存のファイルやリンクを確認して削除
+    if [ -L "$dest" ] || [ -e "$dest" ]; then
+        rm -rf "$dest"
+    fi
+
+    # シンボリックリンクを作成
+    ln -fs "$src" "$dest"
+}
+
+# ホームディレクトリの設定
+create_symlink "$DOT/zsh/.zshrc" "$HOME/.zshrc"
+create_symlink "$DOT/.tmux" "$HOME/.tmux"
+create_symlink "$DOT/.tmux/.tmux.conf" "$HOME/.tmux.conf"
+create_symlink "$DOT/fzf/.fzf.zsh" "$HOME/.fzf.zsh"
+create_symlink "$DOT/.gitconfig" "$HOME/.gitconfig"
 
 # .configディレクトリの設定ファイル
 CONF="$HOME/.config"
@@ -16,8 +30,7 @@ CONF="$HOME/.config"
 # .configディレクトリが存在しない場合は作成
 mkdir -p "$CONF"
 
-ln -fs "$DOT/nvim" "$CONF/nvim"
-ln -fs "$DOT/sheldon" "$CONF/sheldon"
-ln -fs "$DOT/wezterm" "$CONF/wezterm"
-ln -fs "$DOT/starship.toml" "$CONF/starship.toml"
-
+create_symlink "$DOT/nvim" "$CONF/nvim"
+create_symlink "$DOT/sheldon" "$CONF/sheldon"
+create_symlink "$DOT/wezterm" "$CONF/wezterm"
+create_symlink "$DOT/starship.toml" "$CONF/starship.toml"
