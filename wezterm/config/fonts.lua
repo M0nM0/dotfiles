64aa24e6bfc78eb3ddf8ff564a4a8a82d2f54773
ez0momonga <keisuke.monma@techouse.jp>
2026-03-16 19:11:41 +0900

    tmp

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -1,8 +1,6 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
--- local font = 'Maple Mono SC NF'
--- local font_family = 'JetBrainsMono Nerd Font'
 local font_family = 'MesloLGS NF'
 local font_size = 13.0
 
@@ -16,5 +14,5 @@ return {
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Light', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
    freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
-   freetype_load_flags = 'NO_HINTING', -- macOS高DPI向け明示的設定
+   freetype_load_flags = platform.is_mac and 'NO_HINTING' or 'DEFAULT',
 }
